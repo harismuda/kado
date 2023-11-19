713cf40e360c80bc42b4dd41ec663130911f5e41
harismuda20 <harismuda20102003@gmail.com>
2023-11-19 20:39:05 +0700

    Bunga

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 onload = () =>{
         document.body.classList.remove("container");
 }; 
+
+var audio = new Audio("Sound/lagu.mp3");
+
+audio.oncanplaythrough = function(){
+audio.play();
+}
+
+audio.loop = true;
+
+audio.onended = function(){
+audio.play();
+}
